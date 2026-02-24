a5632885c0ed9cfd83c223194cd326da54b759a5
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-02-24 15:30:54 -0300

    primeiros exercicios

---
+++ exercicio-01-estudo-pessoal/exercicio-01-estudo-pessoal/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.ComponentModel;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+using System.Numerics;
+
+/// NÍVEL 1 - FUNDAÇÕES(Conceitos básicos)
+//Exercício 1: Calculadora de Saudação
+//Crie um programa que:
+
+//Use Console.Write para perguntar o nome do usuário
+
+//Use Console.ReadLine para capturar o nome
+
+//Armazene em uma variável string
+
+//Use Console.WriteLine para exibir "Olá, [nome]! Seja bem-vindo!"
+
+//csharp
+//// Exemplo de saída:
+//// Digite seu nome: João
+//// Olá, João! Seja bem-vindo!
+//Exercício 2: Verificador de Idade
+//Peça ao usuário para digitar a idade (use int)
+
+//Use if/else para verificar:
+
+//Se idade >= 18: "Você é maior de idade"
+
+//Se idade< 18: "Você é menor de idade"
+
+//Use Console.Clear para limpar a tela antes de mostrar o resultado
+
+//Exercício 3: Calculadora de Operações Básicas
+//Peça dois números inteiros
+
+//Peça a operação desejada (+, -, *, /)
+
+//Use switch para executar a operação correta
+
+//Mostre o resultado usando Console.WriteLine
+
+
+Console.WriteLine("Bem vindo ao 'Tarefas por Etapa'\n");
+
+
+Console.Write("1 - Digite o seu nome: ");
+string nome = Console.ReadLine()!;
+Console.Clear();
+Console.WriteLine($"Olá {nome}, sejá bem-vindo!\n");
+
+
+
+Console.Write("2 - Digite a sua idade: ");
+string idade = Console.ReadLine()!;
+int idadeNumero = int.Parse(idade);
+
+if (idadeNumero >= 18)
+{
+    Console.WriteLine($"Sua idade '{idadeNumero} anos' te faz uma pessoa maior de idade");
+}
+else
+{
+    Console.WriteLine($"\nSua idade '{idadeNumero} anos' te faz uma pessoa menor de idade");
+}
+
+Thread.Sleep(2500);
+Console.Clear();
+
+Console.WriteLine("Bem vindo a calculadora de Operações!");
+Console.WriteLine("************************************");
+    ;
+Console.Write("\nDigite o primeiro número: ");
+string primeiroNumero = Console.ReadLine()!;
+double primeiroNumeroDouble = double.Parse(primeiroNumero);
+
+Console.Write("Escolha uma das seguints operações -> (+, -, *, /): ");
+string operacao = Console.ReadLine()!;
+
+Console.Write("Digite o segundo número: ");
+string segundoNumero = Console.ReadLine()!;
+double segundoNumeroDouble = double.Parse(segundoNumero);
+
+
+switch (operacao)
+{
+    case "+":
+        double totalSoma = primeiroNumeroDouble + segundoNumeroDouble;
+        Console.WriteLine($"= {totalSoma}");
+        break;
+
+    case "-":
+        double totalSub = primeiroNumeroDouble - segundoNumeroDouble;
+        Console.WriteLine($"= {totalSub}");
+        break;
+
+    case "*":
+        double totalMult = primeiroNumeroDouble * segundoNumeroDouble;
+        Console.WriteLine($"= {totalMult}");
+        break;
+
+    case "/":
+        double totalDiv = primeiroNumeroDouble / segundoNumeroDouble;
+        Console.WriteLine($"= {totalDiv}");
+        break;
+
+    default:
+        Console.WriteLine("Operação Inválida. Por favor, digite uma das existentes.");
+        break;
+
+}
---
+++ primeiro-projeto/primeiro-projeto/Program.cs
@@ -0,0 +1,139 @@
+﻿// Screen Sound
+using System.Data;
+using System.Runtime.CompilerServices;
+
+
+string mensagemDeBoasVindas = "\nBoas vindas ao Screen Sound!";
+//List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso"};
+
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Link Park", new List<int> {10, 8, 6});
+bandasRegistradas.Add("Teh Beatles", new List<int>());
+
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: RegistrarBanda();
+            break;
+        case 2:
+            MostrarBandasRegistradas();
+            break;
+        case 3:
+            AvaliarUmaBanda();
+            break;
+        case 4:
+            Console.WriteLine($"Você escolheu a opção = {opcaoEscolhidaNumerica}");
+            break;
+        case -1:
+            Console.WriteLine("Até logo! :)");
+            break;
+
+        default:
+            Console.WriteLine("Opção Inválida");
+            break;
+    }
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();//LIMPA O TERMINAL
+    ExibirTituloDaOpcao("Exibindo todas as bandas registradas");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    bandasRegistradas.Add(nomeDaBanda, new List<int>());
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000); //DELAY PROPOSITAL 
+    Console.Clear(); //LIMPA O TERMINAL
+    ExibirOpcoesDoMenu(); //EXIBE AS OPÇÕES DO MENU
+}
+
+void MostrarBandasRegistradas()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Registro das Bandas");
+    //for (int i = 0; i < listaDasBandas.Count; i++)
+    //{
+    //    Console.WriteLine($"Banda: {listaDasBandas[i]}");
+    //}
+
+    foreach (string banda in bandasRegistradas.Keys)
+    {
+      Console.WriteLine($"Banda: {banda}");
+    }
+
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void ExibirTituloDaOpcao(string titulo)
+{
+    int quantidadeDeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+
+}
+
+void AvaliarUmaBanda()
+{
+    //digite qual banda deseja avaliar
+    //se a banda existir no dictionary >> atribuir uma nota
+    //senão volta ao menu principal
+    Console.Clear();
+    ExibirTituloDaOpcao("Avaliar Banda");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    string nomeDaBanda = Console.ReadLine();
+
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        Console.Write($"Qual a nota da banda {nomeDaBanda}");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(nota);
+        Console.WriteLine("A nota para foi registrada com sucesso");
+        Thread.Sleep(2000);
+        ExibirOpcoesDoMenu();
+    }
+    else
+    {
+        Console.WriteLine($"A banda {nomeDaBanda} não foi encontrada!");
+        Console.WriteLine("Digite um tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+
+    }
+
+
+}
+
+ExibirLogo();
+ExibirOpcoesDoMenu();
