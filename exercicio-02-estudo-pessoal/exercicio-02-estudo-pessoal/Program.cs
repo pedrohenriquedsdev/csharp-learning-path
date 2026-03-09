9b234a2297f13a033dab4dc43e411f941cead0ab
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-09 20:19:17 -0300

    Exercícios de Lógica de Programaçaõ

--- exercicio-02-estudo-pessoal/exercicio-02-estudo-pessoal/Program.cs
+++ exercicio-02-estudo-pessoal/exercicio-02-estudo-pessoal/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex 6.1 — Percorra os números de 1 a 20. Use continue para pular os pares e exibir apenas os ímpares.
+
+for (int i = 1; i <= 20; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"Número par = {i}");
+    }
+    else
+    {
+        continue;
+    }
+}
+
+Console.WriteLine("=====================");
+
+// Ex 6.2 — Percorra os números de 1 a 100. Use break para parar assim que encontrar o primeiro número divisível por 7 e por 3 ao mesmo tempo.
+
+for (int j = 1; j <= 100; j++)
+{
+    if (j % 7 == 0 && j % 3 == 0)
+    {
+        Console.WriteLine($"O numero divisel por 7 e 3 é = {j}");
+    }
+}
